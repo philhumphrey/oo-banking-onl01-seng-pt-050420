ad6d561c378bbd75506dfeec1547edfa7d0bf920
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-05 05:33:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,10 +15,9 @@ class Transfer
   
   def execute_transaction
   if sender.balance > amount && status == "pending"
-    
-    
+    @sender.balance -= amount
+    @receiver.balance += amount
     @status = "complete"
-    
   end
 end
 
