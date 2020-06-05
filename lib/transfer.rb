a95eed04b01f12340024e5b7aadb1229fdec5d3f
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-05 05:40:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,11 +14,11 @@ class Transfer
   end
   
   def execute_transaction
-  if sender.balance > @amount && status == "pending"
+  if @sender.balance > @amount && status == "pending"
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
-  else @status == "rejected"
+  else @status = "rejected"
   return "Transaction rejected. Please check your account balance."
   end
   end
