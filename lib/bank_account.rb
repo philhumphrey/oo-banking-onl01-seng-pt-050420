1c657c2915a57b37d196943d96aa21fc22d28bc0
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-05 05:15:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
 
+attr_accessor :balance, :status
+attr_reader :name
+
+def initialize (account_name)
+  # @bank_account = bank_account
+  @name = account_name
+  @balance = 1000
+  @status = "open"
 end
+
+def deposit(money)
+  @balance += money
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+  @status == "open" && @balance > 0
+  end
+  
+  def close_account
+    
+    @status = "closed"
+  end
+end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid? 
+  end
+  
+  def execute_transaction
+    sender.valid
+    
+    @status = "complete"
+    
+  end
 end
+
